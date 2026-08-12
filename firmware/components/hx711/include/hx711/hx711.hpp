3db127312607f551df977c738dfe6a303cb0bf18
Vitor Pietrobom <53837251+VitorPietrobom@users.noreply.github.com>
2026-08-12 01:25:42 -0300

    Add HX711 raw hardware test

--- firmware/components/hx711/include/hx711/hx711.hpp
+++ firmware/components/hx711/include/hx711/hx711.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "driver/gpio.h"
 #include "esp_check.h"
 #include "esp_err.h"
@@ -10,6 +12,8 @@ class Hx711 final {
 public:
     Hx711(gpio_num_t data_pin, gpio_num_t clock_pin);
     esp_err_t initialize();
+    bool is_ready() const;
+    esp_err_t read_raw(int32_t& value, uint32_t timeout_ms = 1000) const;
 
 private:
     gpio_num_t data_pin_;
